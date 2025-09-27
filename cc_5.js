02ec6cb85af13dda0834f25d57eb7931db31feb5
Ariella <ariellayavno@usf.edu>
2025-09-27 17:05:44 -0400

    first commit

---
+++ cc_5.js
@@ -0,0 +1,35 @@
+// Step 2: Create an array of 3–5 employee objects
+
+const employee = [
+    { name: "John Smith", hourlyRate: 30, hoursWorked: 50},
+    { name: "Karen Johnson", hourlyRate: 35, hoursWorked: 40},
+    { name: "Josh Watson", hourlyRate: 42, hoursWorked: 45}
+];
+
+// Step 3: Write a function calculateBasePay(rate, hours)
+
+function calculateBasePay(rate, hours) {
+    let maxHours = 40;
+    let otherHours;
+
+    if (hours > maxHours) {
+    otherHours = maxHours;
+  } else {
+    otherHours = hours;
+  }
+  return rate * otherHours;
+};
+
+// Step 4: Write calculateOvertimePay(rate, hours)
+
+function calculateOvertimePay(rate, hours) {
+    let maxHours = 40;
+  let overtimeHours = 0;
+
+  if (hours > maxHours) {
+    overtimeHours = hours - maxHours;
+  }
+
+  return (rate * 1.5) * overtimeHours;
+};
+
